8c644c43fd80d7fa660ce628b512d12bd0a270a5
Rakibul Hassan <117431443+razorrakib@users.noreply.github.com>
2025-06-02 21:31:06 +0600

    Add files via upload

---
+++ Hackerrank Assignment 1/Multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    long long int x, y;
+    scanf("%lli %lli", &x, &y);
+
+    long long int mul = x * y;
+
+    printf("%lli", mul);
+
+    return 0;
+}
---
+++ Hackerrank Assignment 1/Print_It.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char p = '%';
+
+    printf("Hello, world! I am learning C programming language. ^_^");
+    printf("\nProgramming is fun and challenging. /\\/\\/\\");
+    printf("\nI want to give my 100%c ", p);
+    printf("dedication to learn!\tI will succeed one day.");
+
+    return 0;
+}
